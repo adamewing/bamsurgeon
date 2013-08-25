649bc379e5ac5cd06884bf023b34a20154a31223
Adam Ewing <adam.ewing@gmail.com>
2013-08-25 01:25:23 -0700

    misc

--- etc/randomsv.py
+++ etc/randomsv.py
@@ -1,16 +1,18 @@
 #!/bin/env python
 
 from random import *
+from os import getcwd
 import sys
 
 def randomsv():
+    pre = getcwd() + '/ins_seqs/'
     i = randint(0,3)
     if i == 0: # DEL
         dfrac = uniform(0.5,1)
         return "DEL " + str(dfrac)
     if i == 1: # INS
         files = ['alu.fa','line.fa','line_trunc.fa','pseudo.fa']
-        insfile = files[randint(0,len(files)-1)]
+        insfile = pre + files[randint(0,len(files)-1)]
         tsdlen = randint(0,30)
         return ' '.join(('INS',insfile,str(tsdlen)))
     if i == 2: # INV
