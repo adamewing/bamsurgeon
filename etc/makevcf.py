7abc94b53332db5c6539b3d88da3164ea196d9eb
Adam Ewing <adam.ewing@gmail.com>
2014-01-23 14:22:44 -0800

    update makevcf.py

--- etc/makevcf.py
+++ etc/makevcf.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
 
 import sys,os
+import textwrap
 
-def header():
-    print '##fileformat=VCFv4.1'
-    print '##INDIVIDUAL=TRUTH'
-    print '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">'
-    print '##INFO=<ID=SOMATIC,Number=0,Type=Flag,Description="Somatic mutation in primary">'
-    print '##INFO=<ID=VAF,Number=1,Type=Float,Description="Variant Allele Frequency">'
-    print '##INFO=<ID=DPR,Number=1,Type=Float,Description="Avg Depth in Region (+/- 1bp)">'
-    print '##SAMPLE=<ID=TRUTH,Individual="TRUTH",Description="Validation data"'
-    print '\t'.join(('#CHROM','POS','ID','REF','ALT','QUAL','FILTER','INFO','FORMAT','TRUTH'))
+def print_header():
+    print textwrap.dedent("""\
+    ##fileformat=VCFv4.1
+    ##phasing=none
+    ##INDIVIDUAL=TRUTH
+    ##SAMPLE=<ID=TRUTH,Individual="TRUTH",Description="bamsurgeon spike-in">
+    ##INFO=<ID=CIPOS,Number=2,Type=Integer,Description="Confidence interval around POS for imprecise variants">
+    ##INFO=<ID=IMPRECISE,Number=0,Type=Flag,Description="Imprecise structural variation">
+    ##INFO=<ID=SVTYPE,Number=1,Type=String,Description="Type of structural variant">
+    ##INFO=<ID=SVLEN,Number=.,Type=Integer,Description="Difference in length between REF and ALT alleles">
+    ##INFO=<ID=SOMATIC,Number=0,Type=Flag,Description="Somatic mutation in primary">
+    ##INFO=<ID=VAF,Number=1,Type=Float,Description="Variant Allele Frequency">
+    ##INFO=<ID=DPR,Number=1,Type=Float,Description="Avg Depth in Region (+/- 1bp)">
+    ##INFO=<ID=MATEID,Number=1,Type=String,Description="Breakend mate">
+    ##ALT=<ID=INV,Description="Inversion">
+    ##ALT=<ID=DUP,Description="Duplication">
+    ##ALT=<ID=DEL,Description="Deletion">
+    ##ALT=<ID=INS,Description="Insertion">
+    ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+    #CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT\tSPIKEIN""")
 
 if len(sys.argv) == 2:
-    header()
+    print_header()
     logdir_files = os.listdir(sys.argv[1])
 
     for filename in logdir_files:
