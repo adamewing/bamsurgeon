837200871e9b850726fdf562217915731fe080ed
Adam Ewing <adam.ewing@gmail.com>
2014-10-17 09:31:40 +1000

    moving duplicated code to common

--- addsnv.py
+++ addsnv.py
@@ -10,6 +10,7 @@ import bs.replacereads as rr
 import datetime
 import traceback
 
+from bs.common import *
 from uuid import uuid4
 from shutil import move
 from re import sub
@@ -20,8 +21,6 @@ from collections import Counter
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 sys.stderr = os.fdopen(sys.stderr.fileno(), 'w', 0)
 
-def now():
-    return str(datetime.datetime.now())
 
 def majorbase(basepile):
     """returns tuple: (major base, count)
@@ -107,56 +106,6 @@ def countBaseAtPos(bamfile, chrom, pos, mutid='null'):
 
     return bases
 
-def mergebams(bamlist, outbamfn, maxopen=1000):
-    """ call samtools to merge two .bams
-    """
-
-    assert outbamfn.endswith('.bam')
-    print "INFO\t" + now() + "\tlen(bamlist)", len(bamlist)
-
-    if len(bamlist) == 1:
-        print "INFO\t" + now() + "\tonly one BAM to merge, renaming",bamlist[0],"-->",outbamfn
-        move(bamlist[0], outbamfn)
-    else:
-        nmerge = 1
-        mergenum = 0
-        merge_sublists = {}
-        for tmpbam in bamlist:
-            mergetmp = "tmp.merging." + str(mergenum) + "." + outbamfn
-            if mergetmp in merge_sublists:
-                merge_sublists[mergetmp].append(tmpbam)
-            else:
-                merge_sublists[mergetmp] = []
-                merge_sublists[mergetmp].append(tmpbam)
-            if nmerge % maxopen == 0:
-                mergenum += 1
-            nmerge += 1
-
-        for submergefn, tmpbams in merge_sublists.iteritems():
-            if len(tmpbams) == 1:
-                move(tmpbams[0], submergefn)
-                print "INFO\t" + now() + "\trenamed:",tmpbams[0],"-->",submergefn
-            else:
-                args = ['samtools','merge','-f',submergefn] + tmpbams 
-                print "INFO\t" + now() + "\tmerging, cmd: ",args
-                subprocess.call(args)
-
-        if len(merge_sublists.keys()) == 1:
-            print "INFO\t" + now() + "\tmerge finished, renaming:",merge_sublists.keys()[0],"-->",outbamfn
-            move(merge_sublists.keys()[0], outbamfn)
-        else:
-            args = ['samtools','merge','-f',outbamfn] + merge_sublists.keys() 
-            print "INFO\t" + now() + "\tfinal merge, cmd: ",args
-            subprocess.call(args)
-
-        for submergefn in merge_sublists.keys():
-            if os.path.exists(submergefn):
-                os.remove(submergefn)
-
-    for bamfile in bamlist:
-        if os.path.exists(bamfile):
-            os.remove(bamfile)
-            os.remove(bamfile + '.bai')
 
 def remap_paired(bamfn, threads, bwaref, mutid='null'):
     """ call bwa/samtools to remap .bam
--- addsv.py
+++ addsv.py
@@ -10,6 +10,7 @@ import bs.asmregion as ar
 import bs.mutableseq as ms
 import datetime
 
+from bs.common import *
 from uuid import uuid4
 from time import sleep
 from shutil import move
@@ -20,10 +21,6 @@ from multiprocessing import Pool
 
 sys.stdout = os.fdopen(sys.stdout.fileno(), 'w', 0)
 sys.stderr = os.fdopen(sys.stderr.fileno(), 'w', 0)
-
-
-def now():
-    return str(datetime.datetime.now())
 
 
 def remap_bwamem(fq1, fq2, threads, bwaref, outbam, deltmp=True, mutid='null'):
@@ -382,57 +379,6 @@ def load_inslib(infa):
 
     return seqdict
 
-
-def mergebams(bamlist, outbamfn, maxopen=100, debug=False):
-    ''' call samtools to merge a list of bams hierarchically '''
-
-    assert outbamfn.endswith('.bam')
-    print "INFO\t" + now() + "\tlen(bamlist)", len(bamlist)
-
-    if len(bamlist) == 1:
-        print "INFO\t" + now() + "\tonly one BAM to merge, renaming",bamlist[0],"-->",outbamfn
-        os.rename(bamlist[0], outbamfn)
-    else:
-        nmerge = 1
-        mergenum = 0
-        merge_sublists = {}
-        for tmpbam in bamlist:
-            mergetmp = "tmp.merging." + str(mergenum) + "." + outbamfn
-            if mergetmp in merge_sublists:
-                merge_sublists[mergetmp].append(tmpbam)
-            else:
-                merge_sublists[mergetmp] = []
-                merge_sublists[mergetmp].append(tmpbam)
-            if nmerge % maxopen == 0:
-                mergenum += 1
-            nmerge += 1
-
-        for submergefn, tmpbams in merge_sublists.iteritems():
-            if len(tmpbams) == 1:
-                os.rename(tmpbams[0], submergefn)
-                print "INFO\t" + now() + "\trenamed:",tmpbams[0], " --> ", submergefn
-            else:
-                args = ['samtools','merge','-f',submergefn] + tmpbams 
-                print "INFO\t" + now() + "\tmerging, cmd: ", args
-                subprocess.call(args)
-
-        if len(merge_sublists.keys()) == 1:
-            print "INFO\t" + now() + "\tmerge finished, renaming: ", merge_sublists.keys()[0]," --> ", outbamfn
-            os.rename(merge_sublists.keys()[0], outbamfn)
-        else:
-            args = ['samtools','merge','-f',outbamfn] + merge_sublists.keys()
-            print "INFO\t" + now() + "\tfinal merge, cmd: ", args
-            subprocess.call(args)
-
-        for submergefn in merge_sublists.keys():
-            if os.path.exists(submergefn):
-                os.remove(submergefn)
-
-    if not debug:
-        for bamfile in bamlist:
-            if os.path.exists(bamfile):
-                os.remove(bamfile)
-                os.remove(bamfile + '.bai')
 
 
 def align(qryseq, refseq):
