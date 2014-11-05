243cb2ce83cc761ce02ec0f6bb3b7d6a79212170
Adam Ewing <adam.ewing@gmail.com>
2014-11-05 16:31:16 +1000

    preliminary bowtie2 supoprt for snv/indel

---
+++ test/test_snv_bowtie2.sh
@@ -0,0 +1,69 @@
+#!/bin/bash
+
+# adds up to 100 SNPs to a ~770 kb region around the LARGE gene
+# requires samtools/bcftools
+
+if [ $# -ne 5 ]
+then
+    echo "usage: $0 <number of SNPs> <number of threads> <reference indexed with bwa index> <path to SamToFastq.jar provided by picard tools> <bowtie2 index>"
+    exit 65
+fi
+
+if [ ! -e ../addsnv.py ]
+then
+    echo "addsnv.py isn't one directory level down (../addsnv.py) as expected"
+    exit 65
+fi
+
+if [ ! -e $4 ]
+then
+    echo "cannot find SamToFastq.jar"
+    exit 65
+fi
+
+if [ ! -e $5 ]
+then
+    echo "cannot find bowtie2 index"
+    exit 65
+fi
+
+if [ ! -e $3 ]
+then
+    echo "can't find reference .fasta: $3, please supply a bwa-indexed .fasta"
+    exit 65
+fi
+
+if [ ! -e $3.bwt ]
+then
+    echo "can't find $3.bwt: is $3 indexed with bwa?"
+    exit 65
+fi
+
+if ! [[ $1 =~ ^[0-9]+$ ]]
+then
+    echo "arg 1 must be an integer (number of SNVs to add)"
+    exit 65
+fi
+
+if [ $1 -gt 100 ]
+then
+    echo "max number of SNVs must be <= 100"
+    exit 65
+fi
+
+if [ $1 -lt 1 ]
+then
+    echo "min number of SNVs must be > 0"
+    exit 65
+fi
+
+../addsnv.py -v ../test_data/random_snvs.txt -f ../test_data/testregion_bt2.bam -r $3 -o ../test_data/testregion_bt2_mut.bam -n $1 -p $2 --samtofastq $4 --aligner bowtie2 --alignopts bowtie2ref:$5
+
+if [ $? -ne 0 ]
+then
+ echo "addsnv.py failed. Are all the prequisites installed?"
+ exit 65
+else
+  samtools mpileup -ugf $3 ../test_data/testregion_bt2_mut.bam | bcftools view -bvcg - > result.raw.bcf
+  bcftools view result.raw.bcf
+fi
