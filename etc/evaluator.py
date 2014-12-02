8dbb22487b40c740c295ec9644d0056665f06e52
Andreas Wilm - CMB5 <wilma@gis.a-star.edu.sg>
2014-12-02 11:26:57 +0800

    Added haplotype aware indel comparions to evaluator, therefore avoiding problems caused by the positional bias of indels

--- etc/evaluator.py
+++ etc/evaluator.py
@@ -4,11 +4,13 @@ import sys, os
 import vcf
 import argparse
 import string
+import pysam
 
 '''
 Evaluate VCFs against BAMSurgeon "Truth" VCFs
 Adam Ewing, adam.ewing@mater.uq.edu.au
 Requires PyVCF (https://github.com/jamescasbon/PyVCF)
+and pysam.
 '''
 
 def match(subrec, trurec, vtype='SNV'):
@@ -83,13 +85,116 @@ def svmask(rec, vcfh, truchroms):
     return False
 
 
-def evaluate(submission, truth, vtype='SNV', ignorechroms=None, ignorepass=False, 
+def var_dist(v1, v2):
+    """compute absolute distance between two variants
+    """
+    assert v1.CHROM == v2.CHROM
+    return abs(v1.POS-v2.POS)
+
+
+def get_close_matches(var, vcf_fh, win, indels_only=True):
+    """Find close matches for variant (PyVCF record var) in file (PyVCF
+    VCFReader vcf_fh) within given window win and return as list of
+    item,distance tuples, sorted ascendingly by distance.
+    
+    """
+    
+    matches = list(vcf_fh.fetch(var.CHROM, var.POS-win, var.POS+1+win))
+    if indels_only:
+        matches = [m for m in matches if m.is_indel]
+    if len(matches) == 0:
+        return []
+        
+    dist_map = [(m, var_dist(m, var)) for m in matches]
+    return sorted(dist_map, key=lambda x: x[1])
+
+
+def have_identical_haplotypes(v1, v2, ref):
+    """Check if two variant produce the same haplotype / variant sequence.
+
+    - v1 and v2: PyVCF variants to compare
+    - ref: PySAM FastaFile
+    """
+
+    assert (v1.is_indel or v1.is_snp) and (v2.is_indel or v2.is_snp)
+
+    if v1.CHROM != v2.CHROM:
+        return False
+
+    if v1.is_snp and v2.is_snp:
+        assert v1.REF.upper() == v2.REF.upper()
+        return str(v1.ALT[0]).upper() == str(v2.ALT[0]).upper()
+    if v1.is_snp or v2.is_snp:
+        # one snp one indel: can't produce identical results
+        return False
+
+    assert v1.is_indel and v2.is_indel
+    # only on allele per variant allowed
+    assert len(v1.ALT) == 1 and len(v2.ALT) == 1, (
+        "Can't handle multi-allelic entries")
+
+    # get the sequence context whic fully overlaps both variants.
+    # note: pyvcf is one-based, but start and end are zero-based half-open
+    start = min([v1.POS, v2.POS])-1
+    end = max([v1.POS + max([len(v1.REF), len(v1.ALT[0])]),
+               v2.POS + max([len(v2.REF), len(v2.ALT[0])])
+           ])
+    chrom = v1.CHROM# made sure before they are identical before
+    seq = list(ref.fetch(chrom, start, end).upper())
+
+    if len(seq) != end-start:
+        # FIXME how to handle
+        LOG.critical("WARN: Couldn't fetch full sequence window. Skipping"
+                     " allele-aware comparison, otherwise indices would"
+                     " be off\n")
+        raise NotImplementedError
+
+    v1_offset = v1.POS-1-start
+    v2_offset = v2.POS-1-start
+    # lower() in replacement for debugging purposes only
+    v1_seq = seq[:v1_offset] + list(str(v1.ALT[0]).lower()) + seq[v1_offset+len(v1.REF):]
+    v2_seq = seq[:v2_offset] + list(str(v2.ALT[0]).lower()) + seq[v2_offset+len(v2.REF):]
+    if False:
+        print "reference sequence context\t%s" % (''.join(seq))
+        print "v1 (offset %d) %s\t%s" % (v1_offset, v1, ''.join(v1_seq))
+        print "v2 (offset %d) %s\t%s" % (v2_offset, v2, ''.join(v2_seq))
+        print
+
+    try:
+        assert seq[v1_offset] == v1.REF[0]
+        assert seq[v2_offset] == v2.REF[0]
+        assert len(v1_seq) == len(seq) - len(v1.REF) + len(v1.ALT[0])
+        assert len(v2_seq) == len(seq) - len(v2.REF) + len(v2.ALT[0])
+    except AssertionError:
+        #import pdb; pdb.set_trace()
+        raise
+
+    return ''.join(v1_seq).upper() == ''.join(v2_seq).upper()
+
+
+def vars_identical(v1, v2):
+    """Return if two variants are "identical" which here means, same
+    chrom, same pos, same alt base
+    
+    FIXME check if pyvcf record cmp is the same
+    """
+    
+    assert len(v1.ALT) == 1 and len(v2.ALT) == 1
+    return all([v1.CHROM == v2.CHROM, v1.POS == v2.POS, v1.REF == v2.REF, v1.ALT[0] == v2.ALT[0]])
+
+
+def evaluate(submission, truth, vtype='SNV', reffa=None, ignorechroms=None, ignorepass=False, 
              fpfh=False, tpfh=False):
     ''' return stats on sensitivity, specificity, balanced accuracy '''
 
     assert vtype in ('SNV', 'SV', 'INDEL')
     subvcfh = vcf.Reader(filename=submission)
     truvcfh = vcf.Reader(filename=truth)
+
+    if reffa:
+        reffa_fh  = pysam.Fastafile(reffa)
+    else:
+        reffa_fh = None
 
     tpcount = 0
     fpcount = 0
@@ -125,11 +230,23 @@ def evaluate(submission, truth, vtype='SNV', ignorechroms=None, ignorepass=False
             startpos, endpos = expand_sv_ends(subrec)
         try:
             if relevant(subrec, vtype, ignorechroms) and passfilter(subrec, disabled=ignorepass) and subrec.CHROM in truchroms:
-                for trurec in truvcfh.fetch(subrec.CHROM, startpos, end=endpos):
-                    if match(subrec, trurec, vtype=vtype) and str(trurec) not in used_truth:
-                        matched = True
+                if subrec.is_indel and reffa_fh:
+                    for (trurec, d) in get_close_matches(subrec, truvcfh, 100, indels_only=True):
+                        if vars_identical(trurec, subrec):
+                            matched = True
+                            break
+                        elif have_identical_haplotypes(subrec, trurec, reffa_fh):
+                            #sys.stderr.write("Rescueing %s which has same haplotype as %s\n" % (subrec, trurec))
+                            matched = True
+                            break
+                    if matched:
                         used_truth[str(trurec)] = True
-
+                else:
+                    for trurec in truvcfh.fetch(subrec.CHROM, startpos, end=endpos):
+                        if match(subrec, trurec, vtype=vtype) and str(trurec) not in used_truth:
+                            matched = True
+                            used_truth[str(trurec)] = True
+                    
         except ValueError as e:
             sys.stderr.write("Warning: " + str(e) + "\n")
 
@@ -204,15 +321,18 @@ def main(args):
         print "Writing TPs to %s" % args.print_tp_to
     else:
         tpfh = False
-    result = evaluate(args.subvcf, args.truvcf, vtype=args.mutype, ignorechroms=chromlist, ignorepass=args.nonpass, 
+
+    result = evaluate(args.subvcf, args.truvcf, vtype=args.mutype, reffa=args.reffa,
+                      ignorechroms=chromlist, ignorepass=args.nonpass, 
                       fpfh=fpfh, tpfh=tpfh)
 
     print "precision, recall, F1 score: " + ','.join(map(str, result))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="check vcf output against a 'truth' vcf")
-    parser.add_argument('-v',  '--vcf',    dest='subvcf', required=True, help="VCF being submitted for evaluation")
-    parser.add_argument('-t',  '--truth',  dest='truvcf', required=True, help="'Truth' VCF containing true positives")
+    parser.add_argument('-f',  '--ref', dest='reffa', help="Reference fasta file (enables haplotype-ware indel comparison and avoid problems due to positional amibiguity)")
+    parser.add_argument('-v',  '--vcf', dest='subvcf', required=True, help="VCF being submitted for evaluation")
+    parser.add_argument('-t',  '--truth', dest='truvcf', required=True, help="'Truth' VCF containing true positives")
     parser.add_argument('-m', '--mutype', dest='mutype', required=True, help="Mutation type: must be either SNV, SV, or INDEL")
     parser.add_argument('--ignore', dest='chromlist', default=None, help="(optional) comma-seperated list of chromosomes to ignore")
     parser.add_argument('--nonpass', dest='nonpass', action="store_true", help="evaluate all records (not just PASS records) in VCF")
