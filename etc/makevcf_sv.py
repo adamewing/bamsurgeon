145c2d265bb3c6c7301aea5e6658f165ba342335
Adam Ewing <adam.ewing@gmail.com>
2015-06-17 10:19:44 +1000

    masking not passing the right number of parameters after translocation update

--- etc/makevcf_sv.py
+++ etc/makevcf_sv.py
@@ -104,7 +104,7 @@ def ignore_interval(mutline, ref):
 
     assert refstart < refend
 
-    printvcf(chrom, refstart, refend, True, 'IGN', refend-refstart, ref)
+    printvcf(chrom, refstart, refend, True, 'IGN', refend-refstart, ref, '.')
     if m[0] == 'trn': printvcf(m[6], int(m[7]), int(m[8]), True, 'IGN', int(m[8])-int(m[7]), ref)
 
 
