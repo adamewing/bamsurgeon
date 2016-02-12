50a23d99f3c640c7459e93ec38c8c662d3e8fc77
Mischa Lundberg <lundberg.mischa@gmail.com>
2016-02-11 23:01:08 -0500

    changes in travis.yml and prerequisites

--- prerequisites.sh
+++ prerequisites.sh
@@ -24,9 +24,9 @@ sudo cp velvet_1.2.10/velveth $HOME/bin
 sudo pip install cython
 sudo pip install pysam
 #wget ftp://ftp.ncbi.nlm.nih.gov/sra/reports/Assembly/GRCh37-HG19_Broad_variant/Homo_sapiens_assembly19.fasta
-git clone https://github.com/lh3/bwa.git
-sudo make -C bwa
-sudo cp bwa/bwa $HOME/bin
+#git clone https://github.com/lh3/bwa.git
+#sudo make -C bwa
+#sudo cp bwa/bwa $HOME/bin
 #cd bamsurgeon
 export PATH=$HOME/bin:$PATH
 
