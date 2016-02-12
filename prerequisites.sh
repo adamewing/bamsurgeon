d49d31e7460f5217959ad6efa575bfe1dcbcac11
Mischa Lundberg <lundberg.mischa@gmail.com>
2016-02-11 23:21:40 -0500

    changed bin directory for isntalling dependencies

--- prerequisites.sh
+++ prerequisites.sh
@@ -11,16 +11,17 @@
 #git clone https://github.com/lh3/bwa.git
 #sudo make -C bwa
 #sudo cp bwa/bwa $HOME/bin
+mkdir bin
 wget https://github.com/broadinstitute/picard/releases/download/1.131/picard-tools-1.131.zip
 unzip picard-tools-1.131.zip
 wget http://ftp.ebi.ac.uk/pub/software/vertebrategenomics/exonerate/exonerate-2.2.0-x86_64.tar.gz
 tar -xvf exonerate-2.2.0-x86_64.tar.gz
-sudo cp -rf exonerate-2.2.0-x86_64/ $HOME
+sudo cp -rf exonerate-2.2.0-x86_64/ bin/
 wget https://www.ebi.ac.uk/~zerbino/velvet/velvet_1.2.10.tgz
 tar xvzf velvet_1.2.10.tgz
 sudo make -C velvet_1.2.10
-sudo cp velvet_1.2.10/velvetg $HOME/bin
-sudo cp velvet_1.2.10/velveth $HOME/bin
+sudo cp velvet_1.2.10/velvetg bin/
+sudo cp velvet_1.2.10/velveth bin/
 sudo pip install cython
 sudo pip install pysam
 #wget ftp://ftp.ncbi.nlm.nih.gov/sra/reports/Assembly/GRCh37-HG19_Broad_variant/Homo_sapiens_assembly19.fasta
@@ -28,7 +29,7 @@ sudo pip install pysam
 #sudo make -C bwa
 #sudo cp bwa/bwa $HOME/bin
 #cd bamsurgeon
-export PATH=$HOME/bin:$PATH
+
 
 #bundle install --path .bundle --quiet --without=development
 
