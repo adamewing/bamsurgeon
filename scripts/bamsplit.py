1d0ac61bd74814bd3af0cefc336e9247277bd73b
mjko1210 <mjko1210@gmail.com>
2016-07-12 14:44:07 -0400

    add --supplementary option

--- scripts/bamsplit.py
+++ scripts/bamsplit.py
@@ -7,7 +7,7 @@ import sys
 from re import sub
 from collections import defaultdict as dd
 
-def split(inbamfn, keep_secondary=False, seed=None):
+def split(inbamfn, keep_secondary=False, keep_supplementary=False, seed=None):
 
     if seed is not None:
         random.seed(seed)
@@ -31,9 +31,10 @@ def split(inbamfn, keep_secondary=False, seed=None):
     reads = dd(list) 
 
     skip_secondary = 0
+    skip_supplementary = 0
 
     for read in inbam.fetch(until_eof=True):
-        if not read.is_secondary or args.secondary:
+        if (read.is_secondary and args.secondary) or (read.is_supplementary and args.supplementary) or (not read.is_supplementary and not read.is_secondary):
             if lastread is not None and read.qname == lastread.qname:
                 reads[read.qname].append(read)
 
@@ -51,8 +52,11 @@ def split(inbamfn, keep_secondary=False, seed=None):
                     del reads[lastread.qname]
                 reads[read.qname].append(read)
             lastread = read
-        else:
+        elif read.is_secondary:
             skip_secondary += 1
+        else:
+            skip_supplementary +=1
+
 
     ob = None
     if random.random() > 0.5:
@@ -67,16 +71,20 @@ def split(inbamfn, keep_secondary=False, seed=None):
     outbam2.close()
     inbam.close()
 
-    if not args.secondary:
+    if not args.secondary and skip_secondary >= 1:
         print "skipped %d secondary reads" % skip_secondary
 
+    if not args.supplementary and skip_supplementary >= 1:
+        print "skipped %d supplementary reads" % skip_supplementary
+
 def main(args):
-    split(args.bam, keep_secondary=args.secondary, seed=args.seed)
+    split(args.bam, keep_secondary=args.secondary, keep_supplementary=args.supplementary,seed=args.seed)
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='split one bam into two')
     parser.add_argument('-b', '--bam', required=True, help='input BAM: must be sorted by readname (e.g. samtools sort -n)')
     parser.add_argument('--secondary', default=False, action='store_true', help='keep secondary alignments')
+    parser.add_argument('--supplementary', default=False, action='store_true', help='keep supplementary alignments')    
     parser.add_argument('--seed', default=None, help='set PRNG seed')
     args = parser.parse_args()
     main(args)
