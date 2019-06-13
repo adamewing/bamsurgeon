8e300377f564935a7e089b53edff80ed79b82c9a
Adam Ewing <adam.ewing@gmail.com>
2019-06-14 09:54:29 +1000

    add script

---
+++ scripts/remove_non_BS.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+
+import sys
+import pysam
+
+import logging
+FORMAT = '%(levelname)s %(asctime)s %(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+if len(sys.argv) == 2:
+    assert sys.argv[1].endswith('.bam')
+    outfn = '.'.join(sys.argv[1].split('.')[:-1]) + '.BSonly.bam'
+
+    logger.info('outputting reads with BS flag to %s' % outfn)
+
+    inbam  = pysam.AlignmentFile(sys.argv[1], 'rb')
+    outbam = pysam.AlignmentFile(outfn, 'wb', template=inbam)
+
+
+    for read in inbam.fetch(until_eof=True):
+        if read.has_tag('BS'):
+            outbam.write(read)
+
+    inbam.close()
+    outbam.close()
+
+else:
+    sys.exit('%s <bamsurgeon .bam file using --tagreads>')
